a2e4fdd63033eeb532a0ac520a9ddc6c5eb54b7a
stefan_oltmann <stefan_oltmann@112400a2-a957-0410-882a-e3db297fb34b>
2008-12-20 11:11:17 +0000

    gildenbuch import

---
+++ gildebuch/inhalt.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link type="text/css" rel="stylesheet" href="index.css" />
+ <title>KungFuMonkeys Gildenbuch</title>
+</head>
+		
+
+	<body>
+		<div class="rechts">
+				<p>
+				rechts
+				</p>
+		</div>
+			
+		<div class="links">
+				<p>
+				links
+				</p>
+		</div>
+	</body>
+	
+</html>
